99d98f57343edb896a8806d04d6139984fc53a3a
Abhi <meabhishek1512@gmail.com>
2024-02-20 10:15:53 +0530

    First Program file added

---
+++ Basic Programs/FirstProgram.java
@@ -0,0 +1,10 @@
+// Define a class named FirstProgram
+class FirstProgram {
+    // Define the main method which is the entry point of the program
+    public static void main(String[] args) {
+        // Print the string "I Rule" followed by a newline to the console
+        System.out.println("I Rule");
+        // Print the string "the World" followed by a newline to the console
+        System.out.println("the World");
+    }
+}
