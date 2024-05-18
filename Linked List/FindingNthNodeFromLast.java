37a3301c67f2406a1044911c80f4f05d749f0c0f
Abhi <meabhishek1512@gmail.com>
2024-05-18 09:50:29 +0530

    find nth node from last program done

---
+++ Linked List/FindingNthNodeFromLast.java
@@ -0,0 +1,70 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    int size = 0;
+
+    LinkedList() {
+        this.head = null;
+    }
+
+    // Method to insert a new node at the end of the linked list
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    // Method to print the linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public int getNodeFromLast(int pos) {
+        int idx = size - pos;
+
+        while (idx != 0) {
+            head = head.next;
+            idx--;
+        }
+        return head.data;
+    }
+}
+
+public class FindingNthNodeFromLast {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+
+        list.display(); // Output: 1 2 3 4 5 6
+        System.out.println(list.getNodeFromLast(3));
+
+    }
+
+}
