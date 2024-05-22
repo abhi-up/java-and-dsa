6a229077bc0a85e72ceeceb07398723fd9023e45
Abhi <meabhishek1512@gmail.com>
2024-05-22 09:46:35 +0530

    LeetCode Middle Of the Linked list program done

---
+++ Linked List/LeetCodeMiddleOfLL.java
@@ -0,0 +1,57 @@
+// 876. Middle of the Linked List
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LeetCodeMiddleOfLL {
+    public static void main(String[] args) {
+        // Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // Find the middle node
+        ListNode middle = Solution.middleNode(head);
+        System.out.println("The middle node's value is: " + middle.val);
+
+        // Create another linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+        ListNode head2 = new ListNode(1);
+        head2.next = new ListNode(2);
+        head2.next.next = new ListNode(3);
+        head2.next.next.next = new ListNode(4);
+        head2.next.next.next.next = new ListNode(5);
+        head2.next.next.next.next.next = new ListNode(6);
+
+        // Find the middle node of the second list
+        ListNode middle2 = Solution.middleNode(head2);
+        System.out.println("The middle node's value is: " + middle2.val);
+    }
+}
+
+class Solution {
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
