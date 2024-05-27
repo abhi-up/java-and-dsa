778e3e179c09492639a4da7aa7b66bb719a78dc2
Abhi <meabhishek1512@gmail.com>
2024-05-27 18:32:57 +0530

    editted tree sample program

--- Trees/TreeSample.java
+++ Trees/TreeSample.java
@@ -1,3 +1,16 @@
+public class TreeSample {
+    public static void main(String[] args) {
+        // Creating the root node with data 7
+        TreeNode root = new TreeNode(7);
+
+        // Creating left child node with data 4 and attaching it to the root
+        root.left = new TreeNode(4);
+
+        // Creating right child node with data 10 and attaching it to the root
+        root.right = new TreeNode(10);
+    }
+}
+
 class TreeNode {
     int data; // Data stored in the node
     TreeNode left; // Reference to the left child node
@@ -11,18 +24,5 @@ class TreeNode {
     // Method to provide a string representation of the node
     public String toString() {
         return "Value= " + data;
-    }
-}
-
-public class TreeSample {
-    public static void main(String[] args) {
-        // Creating the root node with data 7
-        TreeNode root = new TreeNode(7);
-
-        // Creating left child node with data 4 and attaching it to the root
-        root.left = new TreeNode(4);
-
-        // Creating right child node with data 10 and attaching it to the root
-        root.right = new TreeNode(10);
     }
 }
